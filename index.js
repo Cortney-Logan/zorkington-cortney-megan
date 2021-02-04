ae2306055e0fd07bcde16b11a026f3ddf50540c9
meganzinka <meganzinka@gmail.com>
2021-02-04 16:09:14 -0500

    Changes made: enter room function, switch statement to trigger other functions

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const { ENETRESET } = require('constants');
+const { read } = require('fs');
+const { exit } = require('process');
 const readline = require('readline');
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
@@ -7,14 +10,78 @@ function ask(questionText) {
   });
 }
 
+//room objects
+
+//item objects
+
+//enter function displays the room description
+function enterRoom(){
+  //stuff that makes you enter room
+}
+
 start();
 
 async function start() {
-  const welcomeMessage = `182 Main St.
-You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
+//declares variable to store current room
+let currentRoom = '';
+  const welcomeMessage = `Welcome to Hogwarts, School of Witchcraft and Wizardry! Today you are tasked with a very important mission: find the remaining horcrux and destroy it to defeat Voldemort once and for all! Are you ready to defeat Voldemort?\n`;
   let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
+  while (answer.toLowerCase().trim() !== 'yes') {
+    answer = await ask("Say yes when you're ready to begin!\n")
+  } 
+   //the user has said yes - show them the room description for the first room
+   enter(greatHall)
+
+  while(answer!=='exit') {
+answer = await ask("_");
+answer = answer.toLowerCase().trim()
+console.log(answer);
+      switch (true) {
+        case answer.includes("move"): move()
+        break; 
+        case answer.includes("look around") : lookAround()
+        break; 
+        case answer.includes("examine"): examine(); 
+        break;
+        case answer.includes("read") : read(); 
+        break; 
+        case answer.includes("greet") : greet(); 
+        break; 
+        case answer.includes("take"): take();
+        break; 
+        case answer.includes("check inventory"): checkInventory();
+        break;
+        
+        default: 
+        console.log(`Sorry, I don't know how to ${answer}.`)
+       }
+      
+    }
+   console.log('Goodbye!');
   process.exit();
 }
+
+greatHall = {
+  status: "unlocked", 
+  roomDescription: "Welcome to the Great Hall! It is filled with students feasting on many treats, including your favorite- treacle tarts!.In the distance you see professor MgGonnagall with the sorting hat. What would you like to do?", 
+  items: ["sorting hat", "treacle tart"], 
+  possibleDirections:["north", "south", "east"], 
+}
+
+function enter (room) {
+  currentRoom = room; 
+  console.log(currentRoom.roomDescription)
+}
+
+function move () {
+  console.log("I'm moving!")
+}
+
+
+//room object template: 
+/*roomName = {
+  status: "locked" "unlocked" 
+  "roomDescription": " ", 
+  items: [ ]  
+  possibleDirections: , 
+}*/
