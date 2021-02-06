46498955895593aec9fcec889c1c01c01b5d9d94
Cortney-Logan <logan.cort@gmail.com)>
2021-02-06 11:40:02 -0500

    organize contents for rooms, items and actions

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function ask(questionText) {
     readlineInterface.question(questionText, resolve);
   });
 }
-//rooms class 
+
+//--------------------Rooms------------------------------//
 
 class Room {
   constructor (isUnlocked, roomDescription, itemsInRoom, north, east, south, west) {
@@ -109,19 +110,146 @@ let roomKey = {
 
 //directionkey 
 
-// items class 
+//---------------------------Items----------------------------//
+let inventory = []
+class checkInventory {
+  constructor(name, description, takeable) {
+    this.name = name,// name of the object
+      this.description = description,// text about the object when player grabs it
+    this.takeable = takeable
+  }
+  moreInfo() {
+    return (this.description)
+  }
+  take() {
+    if (this.takeable) {
+      inventory.push(this.name)
+      return ('you have picked ' + this.name + ', you have ' + inventory)
+    }
+    else {
+      return ("you can't take that " + this.description)
+    }
+    
+  }
+
+ 
+}
+
+let diary = new checkInventory('diary', 'Tom Riddle’s Diary', true)
+// console.log(diary.take())
+let scroll = new checkInventory('scroll', 'Instructions', false)
+//console.log(scroll.take())
+let fabric = new checkInventory('fabric', 'description',true)
+//console.log(fabric.take())
+let Horcrux = new checkInventory('Horcrux', 'description', false)
+//console.log(Horcrux.take())
+let TreacleTart = new checkInventory('Treacle Tart', 'Tom Riddle’s Diary', true)
+//console.log(TreacleTart.take())
+let sortingHat = new checkInventory('Sorting Hat', 'Tom Riddle’s Diary', false)
+//console.log(sortingHat.take())
+let book = new checkInventory('Book', 'Tom Riddle’s Diary', true)
+//console.log(book.take())
+let paper = new checkInventory('Paper', 'Tom Riddle’s Diary', false)
+//console.log(paper.take())
 
 
-//items key 
+//items key
+//define items within the item object
+let mapOfItems = {
+  treacletart: "treacleTart",
+  "treacle tart": "treacleTart",
+  tart: "treacleTart",
+  sortinghat: "sortingHat",
+  "sorting hat": "sortingHat",
+  hat: "sortingHat",
+  diary: "riddleDiary",
+  "tom riddle's diary": "riddleDiary",
+  "riddle's diary": "riddleDiary",
+  "tom's diary": "riddleDiary",
+  "riddle diary": "riddleDiary",
+  "tom riddle diary": "riddleDiary",
+  n: "north",
+  s: "south",
+  e: "east",
+  w: "west",
+  north: "north",
+  south: "south",
+  east: "east",
+  west: "west",
+};
 
 
-//actions key 
+//--------------------------------------------Actions---------------------------------------//
+//action key
+let listOfActions = [
+  "move",
+  "look around",
+  "take",
+  "inventory",
+  "examine",
+  "read",
+  "greet",
+];
 
+//drop function
+//would like to drop an item
+// yes
+//which item
+//1=diary, 2= scroll.....
+//player inputs a number of the item to be dropped
+
+async function drop()
+{
+  console.log("which item would you like to drop?")
+  
+  for (i=0;i<inventory.length;i++){
+  console.log(i +"-" +inventory[i])
+}
+userInp= await ask('please input the item number to be removed: ')
+
+  inventory.splice(parseInt(userInp),1)// index,how many item to be removed
+  console.log( 'you have ' + inventory)
+}
 
 //state machine //Megan made this as a statehold
 
 
+//----------------------------------------Functions-------------------------------//
 
+//function to turn answer into array to isolate verb and noun
+function answerToArr(answer) {
+  let answerArr = [];
+  //the first word in the answer should be the action - make that the first element by slicing at the first space
+  answerArr[0] = answer.slice(0, answer.indexOf(" "));
+  //the remaining part of the answer is the noun
+  answerArr[1] = answer.slice(answer.indexOf(" ") + 1);
+  return answerArr;
+}
+
+// //trims the answer to reveal the item called upon by removing each of the action verbs listed in the listOfActions array
+// function isolateItem(answer) {
+//   listOfActions.forEach(
+//     (action) => (answer = answer.replace(action + " ", ""))
+//   );
+//   return answer;
+// }
+
+//returns true or false if the item is found in the mapOfItems
+function validateItem(item) {
+  if (Object.keys(mapOfItems).find((word) => word === item)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//confirms if the item being called upon is in the room the users is currently in
+function checkRoom(item, currentRoom) {
+  return items[mapOfItems[item]].currentRoom === currentRoom;
+}
+
+
+//----------------------Play game-------------------------------------------------//
 start();
 async function start() {
   //declares variable to store current room
@@ -221,196 +349,48 @@ async function start() {
 
 }
 
-//function to turn answer into array to isolate verb and noun
-function answerToArr(answer) {
-  let answerArr = [];
-  //the first word in the answer should be the action - make that the first element by slicing at the first space
-  answerArr[0] = answer.slice(0, answer.indexOf(" "));
-  //the remaining part of the answer is the noun
-  answerArr[1] = answer.slice(answer.indexOf(" ") + 1);
-  return answerArr;
-}
-
-//list of all the rooms and their qualities
-
-
-//define items within the item object
-items = {
-  sortingHat: {
-    description: "here is a description for the sorting hat",
-    movability: "permanent",
-    currentRoom: "greatHall",
-    examine: () => {
-      return "\"Oh you may not think I'm pretty, but don't judge on what you see, I'll eat myself if you can find a smarter hat than me.\"";
-    },
-    read: () => {
-      return "You cannot read the Sorting Hat";
-    },
-    greet: () => {
-      return "Hello. I am the Sorting Hat.  I have a clue for you!  The answer lies in the secret chamber south of this great room.  To enter you must pocess that which will make you invisible to those around you.";
-    },
-    //include take, drop, use(?)
-  },
-  treacleTart: {
-    description: "here is a description for the treacle tart",
-    movability: "removable",
-    currentRoom: "greatHall",
-    examine: () => {
-      return "Hogwart's treacle tarts are made to perfection with a flakey pastry and golden syrup; and of course no soggy bottoms.";
-    },
-    read: () => {
-      return "You cannot read the treacle tart";
-    },
-    greet: () => {
-      return "Not everything in Hogwarts is living...you can't greet a treacle tart.";
-    },
-  },
-  riddleDiary: {
-    description: "here is a description for Tom Riddle's Diary",
-    movability: "removable",
-    currentRoom: "gryffindorCommonRoom",
-    examine: () => {
-      return "You are examining the diary";
-    },
-    read: () => {
-      return "You are reading the diary";
-    },
-    greet: () => {
-      return "Hello. I am the diary of Tom Riddle";
-    },
-  },
-};
-
-//map of items
-let mapOfItems = {
-  treacletart: "treacleTart",
-  "treacle tart": "treacleTart",
-  tart: "treacleTart",
-  sortinghat: "sortingHat",
-  "sorting hat": "sortingHat",
-  hat: "sortingHat",
-  diary: "riddleDiary",
-  "tom riddle's diary": "riddleDiary",
-  "riddle's diary": "riddleDiary",
-  "tom's diary": "riddleDiary",
-  "riddle diary": "riddleDiary",
-  "tom riddle diary": "riddleDiary",
-  n: "north",
-  s: "south",
-  e: "east",
-  w: "west",
-  north: "north",
-  south: "south",
-  east: "east",
-  west: "west",
-};
-
-//list of actions
-let listOfActions = [
-  "move",
-  "look around",
-  "take",
-  "inventory",
-  "examine",
-  "read",
-  "greet",
-];
-
-// //trims the answer to reveal the item called upon by removing each of the action verbs listed in the listOfActions array
-// function isolateItem(answer) {
-//   listOfActions.forEach(
-//     (action) => (answer = answer.replace(action + " ", ""))
-//   );
-//   return answer;
-// }
-
-//returns true or false if the item is found in the mapOfItems
-function validateItem(item) {
-  if (Object.keys(mapOfItems).find((word) => word === item)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-//confirms if the item being called upon is in the room the users is currently in
-function checkRoom(item, currentRoom) {
-  return items[mapOfItems[item]].currentRoom === currentRoom;
-}
-
-//enter room function
-
-//move function
-
-//room object template:
-/*roomName = {
-  status: "locked" "unlocked"
-  "roomDescription": " ",
-  items: [ ]
-  possibleDirections: ,
-}*/
-
-//create item class
-/*let inventory = []
-class checkInventory {
-  constructor(name, description, takeable) {
-    this.name = name,// name of the object
-      this.description = description,// text about the object when player grabs it
-    this.takeable = takeable
-  }
-  moreInfo() {
-    return (this.description)
-  }
-  take() {
-    if (this.takeable) {
-      inventory.push(this.name)
-      return ('you have picked ' + this.name + ', you have ' + inventory)
-    }
-    else {
-      return ("you can't take that " + this.description)
-    }
-    
-  }
-
-
-}
-
-let diary = new checkInventory('diary', 'Tom Riddle’s Diary', true)
-console.log(diary.take())
-let scroll = new checkInventory('scroll', 'Instructions', false)
-console.log(scroll.take())
-let fabric = new checkInventory('fabric', 'description',true)
-console.log(fabric.take())
-let Horcrux = new checkInventory('Horcrux', 'description', false)
-console.log(Horcrux.take())
-let TreacleTart = new checkInventory('Treacle Tart', 'Tom Riddle’s Diary', true)
-console.log(TreacleTart.take())
-let sortingHat = new checkInventory('Sorting Hat', 'Tom Riddle’s Diary', false)
-console.log(sortingHat.take())
-let book = new checkInventory('Book', 'Tom Riddle’s Diary', true)
-console.log(book.take())
-let paper = new checkInventory('Paper', 'Tom Riddle’s Diary', false)
-console.log(paper.take())
-
-drop();
-
-
-//would like to drop an item
-// yes
-//which item
-//1=diary, 2= scroll.....
-//player inputs a number of the item to be dropped
-
- async function drop()
-{
-  console.log("which item would you like to drop?")
-  
-  for (i=0;i<inventory.length;i++){
-  console.log(i +"-" +inventory[i])
-}
-userInp= await ask('please input the item number to be removed: ')
-
-  inventory.splice(parseInt(userInp),1)// index,how many item to be removed
-  console.log( 'you have ' + inventory)
-}
-*/
+// items = {
+//   sortingHat: {
+//     description: "here is a description for the sorting hat",
+//     movability: "permanent",
+//     currentRoom: "greatHall",
+//     examine: () => {
+//       return "\"Oh you may not think I'm pretty, but don't judge on what you see, I'll eat myself if you can find a smarter hat than me.\"";
+//     },
+//     read: () => {
+//       return "You cannot read the Sorting Hat";
+//     },
+//     greet: () => {
+//       return "Hello. I am the Sorting Hat.  I have a clue for you!  The answer lies in the secret chamber south of this great room.  To enter you must pocess that which will make you invisible to those around you.";
+//     },
+//     //include take, drop, use(?)
+//   },
+//   treacleTart: {
+//     description: "here is a description for the treacle tart",
+//     movability: "removable",
+//     currentRoom: "greatHall",
+//     examine: () => {
+//       return "Hogwart's treacle tarts are made to perfection with a flakey pastry and golden syrup; and of course no soggy bottoms.";
+//     },
+//     read: () => {
+//       return "You cannot read the treacle tart";
+//     },
+//     greet: () => {
+//       return "Not everything in Hogwarts is living...you can't greet a treacle tart.";
+//     },
+//   },
+//   riddleDiary: {
+//     description: "here is a description for Tom Riddle's Diary",
+//     movability: "removable",
+//     currentRoom: "gryffindorCommonRoom",
+//     examine: () => {
+//       return "You are examining the diary";
+//     },
+//     read: () => {
+//       return "You are reading the diary";
+//     },
+//     greet: () => {
+//       return "Hello. I am the diary of Tom Riddle";
+//     },
+//   },
+// };
