bc97235c92eb827769a1a49d1e421000438e54f2
Cortney Logan <logan.cort@gmail.com>
2021-04-23 13:18:07 -0400

    modifying server

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static("./client/public"));
+app.use(express.static("./client/build"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve("./client/public/index.html"));
+  res.sendFile(path.resolve("./client/build/index.html"));
 });
 
 app.listen(port, () => {
